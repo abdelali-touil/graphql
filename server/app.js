16ebbe7efb1b8e349175a35e9def63ce065eb12e
Abdelali Touil <touil.abdelali@gmail.com>
2019-07-14 23:36:12 +0100

    initial commit

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const graphqlHttp = require('express-graphql')
+const schema = require('./graphql/schema')
+const mongoose = require('mongoose')
+
+const app = express()
+
+
+mongoose.connect('mongodb://localhost:27017/graphql', { useNewUrlParser: true });
+mongoose.connection.once('open', () => {
+    console.log('connected to database')
+})
+
+app.use('/graphql', graphqlHttp({
+    schema,
+    graphiql: true
+}))
+
+app.listen(4000, () => {
+    console.log('now listening for requests on port 4000')
+});
